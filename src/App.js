4691026d41af28e34248aca10eabad73d50be290
gavrileyes <gavriley-es@mail.ru>
2021-12-11 19:40:28 +0300

    cleare garbage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Сделайте свою тестовую ветку: <code>git branch testing_ИМЯ</code>
         </p>
