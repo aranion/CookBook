357e39f5c5118a0be68c593fd5346503c65af144
Sp1riT42 <pohunov@gmail.com>
2021-12-30 15:51:44 +0300

    Добавил ограничение описания рецепта в 60 символов

--- src/components/RecipeCard/index.tsx
+++ src/components/RecipeCard/index.tsx
@@ -1,48 +1,59 @@
-import { 
+import {
   Card,
   CardHeader,
   CardMedia,
   CardContent,
   CardActions,
-  Avatar, 
-  IconButton, 
+  Avatar,
+  IconButton,
   Typography,
-  Button, 
-  Box, 
-  Rating
-} from '@mui/material';
-import { red } from '@mui/material/colors';
-import FavoriteIcon from '@mui/icons-material/Favorite';
+  Button,
+  Box,
+  Rating,
+} from "@mui/material";
+import { red } from "@mui/material/colors";
+import FavoriteIcon from "@mui/icons-material/Favorite";
 import { IRecipe } from "models/Recipe";
 import imgDefaultGB from "../../assets/cbDefault.jpg";
-import { useActions } from 'hooks/useActions';
-import { ContexMenuDescription } from 'components/Simples';
+import { useActions } from "hooks/useActions";
+import { ContexMenuDescription } from "components/Simples";
 
-export const RecipeCard = ({ recipe }: { recipe: IRecipe })  => {
-
+export const RecipeCard = ({ recipe }: { recipe: IRecipe }) => {
   const { setIsModal } = useActions();
-  
+
   const onOpenModal = (idRecipe: string) => setIsModal(idRecipe);
+  const getMinDescription = (value: number): string => {
+    let str: string = recipe.description.substring(0, value);
+    str += "...";
+    return str;
+  };
 
   return (
-    <Card sx={{ 
-        width: 800, 
-        display: 'flex', 
-        borderRadius: '12px',
-        background: '#f4f4f4', 
-        boxShadow: '0px 4px 20px rgba(0, 0, 0, 0.25)', 
-        margin: '20px auto',
-        overflow: 'hidden',
-        height: '280px'
-    }}>
+    <Card
+      sx={{
+        width: 800,
+        display: "flex",
+        borderRadius: "12px",
+        background: "#f4f4f4",
+        boxShadow: "0px 4px 20px rgba(0, 0, 0, 0.25)",
+        margin: "20px auto",
+        overflow: "hidden",
+        height: "280px",
+      }}
+    >
       <CardMedia
         component="img"
         image={recipe?.urlImg || imgDefaultGB}
         alt={recipe?.title}
-        sx={{ maxHeight: '100%', maxWidth: 300 }}
+        sx={{ maxHeight: "100%", maxWidth: 300 }}
       />
       <Box
-        sx={{ width: 500, display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}
+        sx={{
+          width: 500,
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-between",
+        }}
       >
         <CardHeader
           avatar={
@@ -50,39 +61,36 @@ export const RecipeCard = ({ recipe }: { recipe: IRecipe })  => {
               R
             </Avatar>
           }
-          action={
-            <ContexMenuDescription recipe={recipe} />
-          }
+          action={<ContexMenuDescription recipe={recipe} />}
           title={recipe.title}
         />
-        <Rating 
-          sx={{ padding: 2 }}
-          name="size-medium" 
-          value={2}
-          readOnly
-        />
+        <Rating sx={{ padding: 2 }} name="size-medium" value={2} readOnly />
         <CardContent
-          sx={{ display: 'flex', flexDirection: 'column', gridGap: 16 }}
+          sx={{ display: "flex", flexDirection: "column", gridGap: 16 }}
         >
           <Typography variant="body1" color="text.secondary">
-            {recipe?.description}
+            {recipe?.description.length > 60
+              ? getMinDescription(59)
+              : recipe.description}
           </Typography>
           <Typography variant="body2" color="text.secondary">
             Время приготовления: {recipe.time} минут
           </Typography>
         </CardContent>
-        <CardActions 
+        <CardActions
           disableSpacing
-          sx={{ display: 'flex', justifyContent: 'space-between' }}
+          sx={{ display: "flex", justifyContent: "space-between" }}
         >
           <Box>
             <IconButton aria-label="add to favorites">
               <FavoriteIcon />
             </IconButton>
           </Box>
-          <Button size="small" onClick={() => onOpenModal(recipe.id)}>Посмотреть рецепт</Button>
+          <Button size="small" onClick={() => onOpenModal(recipe.id)}>
+            Посмотреть рецепт
+          </Button>
         </CardActions>
       </Box>
     </Card>
   );
-}
+};
