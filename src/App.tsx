4b37289c7d665ed98cd8e1625db4322b1e29c8ca
gavrileyes <gavriley-es@mail.ru>
2021-12-30 14:16:41 +0300

    add modal component DescriptionRecipe in App

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import {Provider} from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { AppRouter } from "./router";
 import store from "./store";
-import { Header } from "components";
+import { DescriptionRecipe, Header } from "components";
 import styles from "./App.module.scss";
 import { ProvideAuth } from "hooks/useAuth";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
@@ -30,7 +30,7 @@ const Main = () => {
         <div className={styles.main}>
             <Header/>
             <AppRouter/>
-            {/* <DescriptionRecipe /> */}
+            <DescriptionRecipe />
         </div>
     );
 }
