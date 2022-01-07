dfeacd92152dd2347b5de1317a75dc384e5b0921
dekhtiaronchik <72033715+dekhtiaronchik@users.noreply.github.com>
2022-01-07 13:09:57 +0100

    Слайд шоу лучших рецептов (#33)

--- src/components/RandomRecipe/index.tsx
+++ src/components/RandomRecipe/index.tsx
@@ -3,45 +3,30 @@ import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import AccessTimeIcon from "@mui/icons-material/AccessTime";
 import FlatwareIcon from "@mui/icons-material/Flatware";
 import MessageIcon from "@mui/icons-material/Message";
-import { Loader, PrintElem } from "..";
+import { PrintElem } from "..";
 import { Ingredients, IRecipe } from "../../models/Recipe";
 import imgDefaultGB from "../../assets/cbDefault.jpg";
 import styles from "./randomRecipe.module.scss";
 import { useActions } from "hooks/useActions";
-import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+import ThumbUpIcon from "@mui/icons-material/ThumbUp";
 
-interface PropsType {
-  recipes: IRecipe[];
-};
-
-export const RandomRecipe = ({ recipes }: PropsType) => {
-
+export const RandomRecipe = ({ recipe }: { recipe: IRecipe }) => {
   const { setIsModal } = useActions();
 
   const onOpen = (idRecipe: string) => setIsModal(idRecipe);
 
-  const getRandomInt = (min: number, max: number) => {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min;
-  }
-
-  const recipe: IRecipe = recipes[ getRandomInt(0, recipes.length) ];
-
-  if(!recipes || recipes.length === 0) return <div className={styles["recipe-block"]}>
-    <Loader />
-  </div>
-
   return (
     <div className={styles["recipe-block"]}>
       <div className={styles["recipe-block__left"]}>
-        <img 
+        <img
           // TODO 'src' тут заглушка для показа фотографий не с сервера, необходимо убрать при работе на сервере
-          src={recipe.urlImg
-            ? '/img/' + recipe.urlImg.split('/')[recipe.urlImg?.split('/').length - 1]
-            : imgDefaultGB
+          src={
+            recipe.urlImg
+              ? "/img/" +
+                recipe.urlImg.split("/")[recipe.urlImg?.split("/").length - 1]
+              : imgDefaultGB
           }
-          alt={recipe?.title} 
+          alt={recipe?.title}
         />
         <div className={styles["recipe-block__meta"]}>
           <div>
@@ -63,17 +48,17 @@ export const RandomRecipe = ({ recipes }: PropsType) => {
         </div>
       </div>
       <div className={styles["recipe-block__right"]}>
-        <h3 className={styles["recipe-block__header"]}>Случайный/Популярный рецепт</h3>
+        <h3 className={styles["recipe-block__header"]}>Лучшие рецепты</h3>
         <div className={styles["recipe-block__info"]}>
           <div className={styles["heading-container"]}>
             <span className={styles["heading-container__header"]}>
               {recipe?.title}
             </span>
-            <PrintElem recipe={recipe}/>
+            <PrintElem recipe={recipe} />
           </div>
           <h4 className={styles["info-container_ingredients"]}>Ингредиенты:</h4>
           <ul>
-            {recipe?.ingredients.map((item: Ingredients, i:number) => (
+            {recipe?.ingredients.map((item: Ingredients, i: number) => (
               <li key={i}>
                 {item.description} - {item.count}
               </li>
@@ -83,11 +68,11 @@ export const RandomRecipe = ({ recipes }: PropsType) => {
         <div className={styles["recipe-block__right_added"]}>
           <div className={styles["recipe-block__right_added-author"]}>
             <AccountCircleIcon />
-            <span>{recipe?.author || 'Автор не указан...'}</span>
+            <span>{recipe?.author || "Автор не указан..."}</span>
           </div>
           <Button size="small" onClick={() => onOpen(recipe._id)}>
             Посмотреть рецепт
-          </Button> 
+          </Button>
         </div>
       </div>
     </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,21 +1,22 @@
-import { RandomRecipe, LastRecipes } from "components";
+import { SlideShow, LastRecipes } from "components";
 import { useActions } from "hooks/useActions";
 import { useEffect } from "react";
 import { useAppSelector } from "store";
-import styles from './pages.module.scss'
+import styles from "./pages.module.scss";
 
 const Home = () => {
   const { fetchAllRecipes } = useActions();
-  
-  useEffect(()=> {
+
+  useEffect(() => {
     fetchAllRecipes();
   }, []);
 
   const recipes = useAppSelector((state) => state.recipes.data);
-  
+  const sortedRecipes = recipes.sort((a, b) => b.rating - a.rating);
+
   return (
     <div className={styles.pages__center}>
-      <RandomRecipe recipes={recipes}/>
+      <SlideShow recipes={sortedRecipes.slice(0, 5)} />
       <LastRecipes recipes={recipes.slice(-3)} />
     </div>
   );
